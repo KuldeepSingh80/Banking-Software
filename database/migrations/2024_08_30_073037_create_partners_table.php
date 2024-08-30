58e4dc0c3a4b72d96317c4634023086f2f09761a
kuldeep <kuldeepsingh.tws@gmail.com>
2024-08-30 13:23:13 +0530

    Add partners migration

---
+++ database/migrations/2024_08_30_073037_create_partners_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePartnersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('partners', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('company_name');
+            $table->string('email');
+            $table->string('mobile');
+            $table->string('address_one');
+            $table->string('address_two');
+            $table->string('city');
+            $table->string('state');
+            $table->string('zip_code');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('partners');
+    }
+}
